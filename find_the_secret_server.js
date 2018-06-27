78b495f088879a2cf8376da14d652aaa66d60f52
Dave Westerveld <Dave.Westerveld@D2L.com>
2018-06-27 14:02:55 -0400

    rename and some cleanup

--- find_the_secret_server.js
+++ find_the_secret_server.js
@@ -1,19 +1,18 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+const router = jsonServer.router('find_the_secret_db.json')
 const middlewares = jsonServer.defaults()
 
 
 function isAuthorized(req){
-  if (req.url == '/profile'){
+  if (req.url.includes('/profile') ){
     return false;
   }
   return true;
 }
 server.use(middlewares)
 server.use((req, res, next) => {
-  if (isAuthorized(req)) { // add your authorization logic here
-    console.log(req.url)
+  if (isAuthorized(req)) {
     next() // continue to JSON Server router
   } else {
     res.sendStatus(401)
@@ -21,5 +20,5 @@ server.use((req, res, next) => {
  })
 server.use(router)
 server.listen(3000, () => {
-  console.log('JSON Server is running')
+  console.log('JSON Server is running. Requests can be accessed at http://localhost:3000/')
 })
